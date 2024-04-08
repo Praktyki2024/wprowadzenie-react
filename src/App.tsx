5e913fdc84c416da3fee451cea3cbd2a3deba4a3
Praktyki2024 <wolowina.powodowac.0w@icloud.com>
2024-04-08 14:13:18 +0200

    dodano use effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import { useReducer, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Hello from './components/Hello';
 import { nauczyciel as nauczycielJSON} from './data/nauczyciele';
 
-const reducer = (n: typeof nauczycielJSON, action: any) => {
-  return {...n, imie: action.imie }
-}
-
 function App() {
-  const [nauczyciel, dispach] = useReducer(reducer, nauczycielJSON)
+  const [nauczyciel, setNauczyciel] = useState(nauczycielJSON)
   const imionaUczniow = nauczyciel.uczniowie.map((uczen) => uczen.imie);
-  const dodajUczen = (imie: string) => {}
-  const zmienImie = (imie: string) => dispach({imie});
+  const dodajUczen = (imie: string) => setNauczyciel({...nauczyciel, uczniowie: [...nauczyciel.uczniowie, {imie}]})
+  const zmienImie = (imie: string) => setNauczyciel({...nauczyciel, imie})
+  useEffect(() => {
+    alert(JSON.stringify(nauczyciel));
+  }, [nauczyciel]);
+  useEffect(() => {
+    console.log("cześć nauczyciel");
+  }, [nauczyciel]);
   return (
     <>
       <Hello imie={nauczyciel.imie} uczniowie={imionaUczniow} onDodajUczen={dodajUczen} onZmienImie={zmienImie}/>
