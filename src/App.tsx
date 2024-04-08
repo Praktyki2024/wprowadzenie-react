953b8db0e76d217d85fb4a22472b28518aec2ca8
Praktyki2024 <wolowina.powodowac.0w@icloud.com>
2024-04-08 14:20:30 +0200

    dodano useMemo

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import './App.css';
 import Hello from './components/Hello';
 import { nauczyciel as nauczycielJSON} from './data/nauczyciele';
 
 function App() {
   const [nauczyciel, setNauczyciel] = useState(nauczycielJSON)
-  const imionaUczniow = nauczyciel.uczniowie.map((uczen) => uczen.imie);
+  const wyliczImionaUczniow = () => nauczyciel.uczniowie.map((uczen) => uczen.imie);
+  const imionaUczniow = useMemo(wyliczImionaUczniow, [nauczyciel]);
+
   const dodajUczen = (imie: string) => setNauczyciel({...nauczyciel, uczniowie: [...nauczyciel.uczniowie, {imie}]})
   const zmienImie = (imie: string) => setNauczyciel({...nauczyciel, imie})
-  useEffect(() => {
-    alert(JSON.stringify(nauczyciel));
-  }, [nauczyciel]);
-  useEffect(() => {
-    console.log("cześć nauczyciel");
-  }, [nauczyciel]);
   return (
     <>
       <Hello imie={nauczyciel.imie} uczniowie={imionaUczniow} onDodajUczen={dodajUczen} onZmienImie={zmienImie}/>
